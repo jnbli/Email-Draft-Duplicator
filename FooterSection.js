11c041881b22546d7023b628baaff4aa0f71101c
JessHua159 <jesseweihuang@gmail.com>
2020-07-17 19:09:24 -0700

    small change

--- FooterSection.js
+++ FooterSection.js
@@ -2,15 +2,14 @@
 function FooterSection(cardName, cardData) {  
   const refreshButton = CardService.newTextButton()
     .setText("Refresh")
-  
-    // JSON.stringify() is used since setParameters() only takes string keys and values.
     .setOnClickAction(CardService.newAction()
                        .setFunctionName("reloadCard")
+                       // JSON.stringify() is used since setParameters() only takes string keys and values.
                        .setParameters({ "cardName": cardName, "cardData": JSON.stringify(cardData) })); 
 
-  const footerButtonSet = CardService.newButtonSet()
+  const buttonSet = CardService.newButtonSet()
     .addButton(refreshButton);
   
   // Card service's fixed footer causes display glitch with selection input, so the footer is created as a section instead.
-  return CardService.newCardSection().addWidget(footerButtonSet);
+  return CardService.newCardSection().addWidget(buttonSet);
 }
